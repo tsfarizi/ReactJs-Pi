da79175b6044604fe54ce4de5e9671e7e20aace1
teuku <teukusalmanfarizi2003@gmail.com>
2025-09-20 00:22:11 +0700

    Switch from BrowserRouter to HashRouter for routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { loadMidtransScript } from "./utils/loadMidtrans.ts";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 loadMidtransScript(import.meta.env.VITE_MIDTRANS_CLIENT_KEY);
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
+
